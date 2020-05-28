8fb5111bcdfd5dbcb5341934a0e3fe78842f0164
garzaro <garzarocleber@gmail.com>
2020-05-28 16:53:34 -0300

    estrutura de repetição

--- src/main/java/com/cleber/useacabeca/laco/While.java
+++ src/main/java/com/cleber/useacabeca/laco/While.java
@@ -6,33 +6,48 @@ public class While {
 	static String nome = "Cleber";
 	static int jovem = idade;
 	static boolean sorveteIsOn = true;
+	static int loop = 1;
 
 	public static void main(String[] args) {
 
 		while (idade < 18) {
-			
+
 			System.out.println(nome + " Voce tem " + idade + " anos, entrada proibida.");
-			//idade = idade + 1;
+			// idade = idade + 1;
 			idade++;
-	
+
 		}
 		System.out.println("-----------------------------------");
-		
-		if(jovem < 18) {
+
+		if (jovem < 18) {
 			System.out.println("Ouwh criança vai tomar sorvete!!!");
-			
-		}else {
+
+		} else {
 			System.out.println("Nadegas a declarar!!!");
 		}
-		
-		while(sorveteIsOn)
-			sorveteIsOn = false; //hãnnnnn????
-			System.out.println("Come sorvete");
-		
-	}
-		
-}
 
+		while (sorveteIsOn)
+			sorveteIsOn = false; // hãnnnnn????
+		System.out.println("Come sorvete");
+		
+		System.out.println("------------------------------------");
+		
+		System.out.println("Antes do loop");
+		
+		while(loop < 5) {
+			System.out.println("Dentro de loop");
+			
+			System.out.println("O valor de loop é " + loop);
+			
+			loop++;
+		}
+		
+		System.out.println("Aqui termina o loop");
+
+	}
+	
+
+}
 
 /*
  * O laço while e o laço for são apenas formas diferentes de uma mesma estrutura
