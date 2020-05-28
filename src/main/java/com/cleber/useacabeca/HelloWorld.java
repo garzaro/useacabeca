9acc2951c53460a7405138edf62d559f0c405ec1
garzaro <garzarocleber@gmail.com>
2020-05-28 12:08:47 -0300

    classe inicial java

--- src/main/java/com/cleber/useacabeca/HelloWorld.java
+++ src/main/java/com/cleber/useacabeca/HelloWorld.java
@@ -3,7 +3,7 @@ package com.cleber.useacabeca;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World, doido!!!");
+		System.out.println("Relow o ordi, gas!!!");
 
 	}
 
