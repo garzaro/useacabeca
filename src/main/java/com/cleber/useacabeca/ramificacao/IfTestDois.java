0a7db3d3f619b660c2633c9aad58d3e682a177e1
garzaro <garzarocleber@gmail.com>
2020-05-28 17:23:51 -0300

    estrutura de repetição

---
+++ src/main/java/com/cleber/useacabeca/ramificacao/IfTestDois.java
@@ -0,0 +1,21 @@
+package com.cleber.useacabeca.ramificacao;
+
+public class IfTestDois {
+	
+	public static void main(String[] args) {
+		
+		Integer cerveja = 10;
+		
+		if (cerveja == 10) {
+			System.out.println("Ainda tem cerveja na geladeira!!!");
+			
+		} else if(cerveja == 0) {
+			System.out.println("Acabou a ceverja!!!");
+
+		}else {
+			System.out.println("Está esgotando a cerveja!!!");
+		}
+		
+	}
+
+}
